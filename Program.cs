a5aa8d8dcb195606e38cf330a6db85a6421ac601
BehnamBehrouzian <behnam.1381.behrouzian@gmail.com>
2024-11-27 10:21:58 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter number 1 :");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("enter number 2 :");
+        int num2 = int.Parse(Console.ReadLine());
+
+        for (int i = num1; i <= num2; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    // تابع برای بررسی اول بودن عدد
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
